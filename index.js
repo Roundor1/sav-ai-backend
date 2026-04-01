6fa8c0a81dbf5f23d503f1a41028f034cc7e8914
Roundor1 <yanishachouf58@gmail.com>
2026-04-01 23:20:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -106,4 +106,5 @@ ${instruction || ""}
   }
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
